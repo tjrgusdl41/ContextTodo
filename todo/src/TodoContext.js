a800a51e7ed52cad0c16cb2408a9c6210d1471a6
tjrgusdl41 <63176593+tjrgusdl41@users.noreply.github.com>
2021-12-09 10:50:22 +0900

    Context만들기
    ..

---
+++ todo/src/TodoContext.js
@@ -0,0 +1,38 @@
+import React, { useReducer } from "react";
+const initialTodos = [
+  {
+    id: 1,
+    text: "프로젝트생성하기",
+    done: true,
+  },
+  {
+    id: 2,
+    text: "컴포넌트 스타일링",
+    done: true,
+  },
+  {
+    id: 3,
+    text: "Context 만들기",
+    done: true,
+  },
+  {
+    id: 4,
+    text: "기능구현",
+    done: true,
+  },
+];
+function todoReducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return state.concat(action.todo);
+    case "TOGGLE":
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    case "REMOVE":
+      return state.filter((todo) => todo.id !== action.id);
+  }
+}
+export function TodoProvider({ children }) {
+  const [state, dispatch] = useReducer(todoReducer, initialTodos);
+}
